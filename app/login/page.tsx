0a8729bcdfd344ee4213c999e8334dd2ebd922a4
Jatin Garg <jatin26garg@gmail.com>
2026-08-08 17:17:47 +0530

    Suspense added in logi page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,12 @@
 "use client"
 
-import React, { useState } from "react"
+import React, { Suspense, useState } from "react"
 import { useEffect } from "react"
 import { useRouter, useSearchParams } from "next/navigation"
 import Link from "next/link"
 
 
-export default function login() {
+function LoginForm() {
     const router = useRouter();
     const searchParams = useSearchParams();
     const redirectUrl =searchParams.get("redirect");
@@ -153,5 +153,16 @@ export default function login() {
                 </div>
             </div>
         </div>
+    )
+}
+export default function LoginPage() {
+    return (
+        <Suspense fallback={
+            <div className="min-h-screen flex items-center justify-center">
+                <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500"></div>
+            </div>
+        }>
+            <LoginForm />
+        </Suspense>
     )
 }
